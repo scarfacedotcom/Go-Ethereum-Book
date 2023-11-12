7854f099d7faf39a145f8f7bfc2fe8bdb0dd0a9a
scarfacedotcom <peteradaaku@gmail.com>
2023-11-12 09:49:33 +0100

    wallet generate

--- account_balance.go
+++ account_balance.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func main() {
+func main3() {
 	client, err := ethclient.Dial("https://cloudflare-eth.com")
 	if err != nil {
 		log.Fatal(err)
@@ -22,20 +22,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Balance", balance) // 25893180161173005034
+	fmt.Println("Balance:", balance) // 25893180161173005034
 
 	blockNumber := big.NewInt(5532993)
 	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Balance at block 5532993", balanceAt) // 25729324269165216042
+	fmt.Println("Balance at block 5532993:", balanceAt) // 25729324269165216042
 
 	fbalance := new(big.Float)
 	fbalance.SetString(balanceAt.String())
 	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-	fmt.Println("ETH Value", ethValue) // 25.729324269165216041
+	fmt.Println("ETH Value:", ethValue) // 25.729324269165216041
 
 	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
-	fmt.Println("Pending Balance", pendingBalance) // 25729324269165216042
+	fmt.Println("Pending Balance:", pendingBalance) // 25729324269165216042
 }
