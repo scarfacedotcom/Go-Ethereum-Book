8591e6a21ae533dcc0753253484ccf9fb58659ba
scarfacedotcom <peteradaaku@gmail.com>
2023-11-13 07:42:08 +0100

    blocks query

--- Accounts/address_check.go
+++ Accounts/address_check.go
@@ -13,8 +13,8 @@ import (
 func main() {
 	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
 
-	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d")) // is valid: true
-	fmt.Printf("is valid: %v\n", re.MatchString("0xZYXb5d4c32345ced77393b3530b1eed0f346429d")) // is valid: false
+	fmt.Printf("is valid: %v\n", re.MatchString("0xe41d2489571d322189246dafabde1f4699f498"))
+	fmt.Printf("is valid: %v\n", re.MatchString("0xe41d2489571d322189246dafa5ebde1f4699f498"))
 
 	client, err := ethclient.Dial("https://cloudflare-eth.com")
 	if err != nil {
@@ -23,23 +23,23 @@ func main() {
 
 	// 0x Protocol Token (ZRX) smart contract address
 	address := common.HexToAddress("0xe41d2489571d322189246dafa5ebde1f4699f498")
-	bytecode, err := client.CodeAt(context.Background(), address, nil) // nil is latest block
+	bytecode, err := client.CodeAt(context.Background(), address, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	isContract := len(bytecode) > 0
 
-	fmt.Printf("is contract: %v\n", isContract) // is contract: true
+	fmt.Printf("is it a Smart contract Account: %v\n", isContract)
 
-	// a random user account address
+	// Scar Faces account address
 	address = common.HexToAddress("0x8e215d06ea7ec1fdb4fc5fd21768f4b34ee92ef4")
-	bytecode, err = client.CodeAt(context.Background(), address, nil) // nil is latest block
+	bytecode, err = client.CodeAt(context.Background(), address, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	isContract = len(bytecode) > 0
 
-	fmt.Printf("is contract: %v\n", isContract) // is contract: false
+	fmt.Printf("is it a Smart contract Account: %v\n", isContract)
 }
