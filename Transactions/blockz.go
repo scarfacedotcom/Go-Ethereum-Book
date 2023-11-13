3111546100d0f6e180ff541a9c18e4d1831ab2df
scarfacedotcom <peteradaaku@gmail.com>
2023-11-13 08:04:59 +0100

    quering block

--- Transactions/blockz.go
+++ Transactions/blockz.go
@@ -20,7 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(header.Number.String()) // 5671744
+	fmt.Println("The Block Header Number is: ", header.Number.String()) // 5671744
 
 	blockNumber := big.NewInt(5671744)
 	block, err := client.BlockByNumber(context.Background(), blockNumber)
@@ -28,16 +28,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(block.Number().Uint64())     // 5671744
-	fmt.Println(block.Time())                // Access the Uint64 field directly
-	fmt.Println(block.Difficulty().Uint64()) // 3217000136609065
-	fmt.Println(block.Hash().Hex())          // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
-	fmt.Println(len(block.Transactions()))   // 144
+	fmt.Println("The Block Header Number is: ", block.Number().Uint64())
+	fmt.Println("The Block Time is: ", block.Time())
+	fmt.Println("The Block Difficulty is: ", block.Difficulty().Uint64())
+	fmt.Println("The Block Hash is: ", block.Hash().Hex())
+	fmt.Println("The Number of Transactions is: ", len(block.Transactions()))
 
 	count, err := client.TransactionCount(context.Background(), block.Hash())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(count) // 144
+	fmt.Println("The Number of Transactions is: ", count)
 }
